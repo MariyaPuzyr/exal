8d778ecc7ac707ca09b82a87ecc212ae1d56a494
MariyaPuzyr <mariyapyzur@gmail.com>
2019-12-27 15:40:54 +0200

    add right sidebar

--- dist/js/main.min.js
+++ dist/js/main.min.js
@@ -1,1 +1,11 @@
-$(document).ready(function () {});
+$(document).ready(function () {
+  $('.header-menu__list-item').hide();
+  $('.header-menu__list-item:first-of-type').show();
+  $('.header-menu__link').click(function(e){
+    e.preventDefault();
+    $(this).closest('li').siblings().children('a').removeClass('active');
+    $(this).addClass('active');
+    $('.header-menu__list').children('div').hide();
+    $($(this).attr('href')).show();
+  })
+});

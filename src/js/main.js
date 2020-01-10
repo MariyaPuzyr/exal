e005acd0830dcb2a72ae98ea5a0a42a42b3bf3eb
MariyaPuzyr <mariyapyzur@gmail.com>
2020-01-10 12:02:34 +0200

    add currency-reserves section

--- src/js/main.js
+++ src/js/main.js
@@ -1,14 +1,31 @@
 $(document).ready(function () {
-  // $('.header-menu__list-item').hide();
-  // $('.header-menu__link').click(function(e){
-  //   e.preventDefault();
-  //   $(this).closest('li').siblings().children('a').removeClass('active');
-  //   $(this).addClass('active');
-  //   $('.header-menu__list').children('div').hide();
-  //   $($(this).attr('href')).show();
-  //   $(this).parents('.header-menu').addClass('active');
-  // })
   $('.header-menu__link').click(function () {
     $(this).parents('.header-menu').addClass('active');
+    // $('.header-menu__link.active').parents('.header-menu').removeClass('active');
+    // var thisLink = $('.header-menu__link');
+    // if(thisLink.hasClass('active')) {
+    //   $(this).removeClass('active');
+    //   console.log('remove');
+    // } else  {
+    //   $(this).addClass('active');
+    //   console.log('add');
+    // }
+    $('.links-list li a').click(function () {
+      var toLoad = $(this).attr('href');
+      $('#content').hide('fast',loadContent);
+      $('#links').append('<span id="load">LOADING...</span>');
+      $('#load').fadeIn('normal');
+      function loadContent() {
+        $('#content').load(toLoad,'',showNewContent())
+      }
+      function showNewContent() {
+        $('#content').show('normal',hideLoader());
+      }
+      function hideLoader() {
+        $('#load').fadeOut('normal');
+      }
+      return false;
+
+    });
   });
 });

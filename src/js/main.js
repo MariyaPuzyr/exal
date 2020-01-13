82362a4eed9c441b1e9e755319c6032a0a258806
MariyaPuzyr <mariyapyzur@gmail.com>
2020-01-13 14:44:53 +0200

    add style for currency-reserves

--- src/js/main.js
+++ src/js/main.js
@@ -2,15 +2,19 @@ $(document).ready(function () {
   $('.header-menu__link').click(function () {
     $(this).parents('.header-menu').addClass('active');
     $('.links-list li a').click(function () {
-      var toLoad = $(this).data('link');
+      // var toLoad = $(this).data('link')+'#content';
+      var toLoad = $(this).data('link')+' #content';
+      console.log(toLoad);
       if (toLoad.length > 0) {
         $('.menu-height').hide(loadContent);
       }
 
       function loadContent() {
-        $('.menu-height').load(toLoad, '')
+        $('.menu-height').load(toLoad, '', showNewContent)
       }
-
+      function showNewContent() {
+        $('#content').show('normal');
+      }
       return false;
     });
   });

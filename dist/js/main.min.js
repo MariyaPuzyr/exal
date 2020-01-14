914cb776fd029d5b580505a4eb28dcbddc67274f
MariyaPuzyr <mariyapyzur@gmail.com>
2020-01-14 14:29:22 +0200

    fix ajax page load

--- dist/js/main.min.js
+++ dist/js/main.min.js
@@ -1,21 +1,24 @@
 $(document).ready(function () {
   $('.header-menu__link').click(function () {
+    var innerContent = $('.content-inner');
     $(this).parents('.header-menu').addClass('active');
+    /* load inner wrapper for pages */
+    $(this).parents('.header-menu').find('.tab-content').append('<div class="content-inner"></div>');
+
+    /* ajax loader pages by click */
     $('.links-list li a').click(function () {
       var toLoad = $(this).data('link') + ' #content';
       if (toLoad.length > 0) {
-        $('.menu-height').hide(loadContent);
-      }
-
-      function loadContent() {
-        $('.menu-height').load(toLoad, '', showNewContent)
-      }
-
-      function showNewContent() {
-        $('#content').show('normal');
+        $(this).parents('.menu-height').hide();
+        /* load page content*/
+        $('.content-inner').show().load(toLoad, '', function () {
+          $('#content').show();
+        });
       }
       return false;
     });
+    innerContent.remove().hide();
+    $('.menu-height').show();
   });
 
   /* add news slider */

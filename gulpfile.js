5ad60fd14b714953d0ae09453ab7e26c90b102d3
MariyaPuzyr <mariyapyzur@gmail.com>
2020-01-17 16:45:59 +0200

    add login page

--- gulpfile.js
+++ gulpfile.js
@@ -130,8 +130,8 @@ gulp.task('main-libs', function () {
   return gulp.src([
     './node_modules/jquery/dist/jquery.min.js',
     './node_modules/bootstrap/dist/js/bootstrap.bundle.min.js',
-    './node_modules/slick-carousel/slick/slick.min.js',
-    './src/js/share42.js'
+    './node_modules/slick-carousel/slick/slick.min.js'
+    // './src/js/share42.js'
   ])
       .pipe(concat('main-libs.min.js'))
       .pipe(gulp.dest(paths.scripts.dist))
